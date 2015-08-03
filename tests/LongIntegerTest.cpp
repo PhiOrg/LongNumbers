4412bb4fe3b7f8c11da4614fbf131a8c479f764b
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-03 04:08:54 +0300

    Added tests for the next constructors: long, long long, unsigned, unsigned long, unsigned long long and LongInteger.

--- tests/LongIntegerTest.cpp
+++ tests/LongIntegerTest.cpp
@@ -15,3 +15,54 @@ BOOST_AUTO_TEST_CASE(TestConstructorInt)
     BOOST_CHECK(!b.GetSignForTests() && b.GetDigitsForTests() == "5");
 }
 
+BOOST_AUTO_TEST_CASE(TestConstructorLong)
+{
+    long number = 123456789;
+    LongInteger a(number);
+    BOOST_CHECK(a.GetSignForTests() && a.GetDigitsForTests() == "123456789");
+
+    LongInteger b(-number);
+    BOOST_CHECK(!b.GetSignForTests() && b.GetDigitsForTests() == "123456789");
+}
+
+BOOST_AUTO_TEST_CASE(TestConstructorLongLong)
+{
+    long long number = 446744073709551616;
+    LongInteger a(number);
+    BOOST_CHECK(a.GetSignForTests() &&
+                a.GetDigitsForTests() == "446744073709551616");
+
+    LongInteger b(-number);
+    BOOST_CHECK(!b.GetSignForTests() &&
+                b.GetDigitsForTests() == "446744073709551616");
+}
+
+BOOST_AUTO_TEST_CASE(TestConstructorUnsigned)
+{
+    unsigned number = 123;
+    LongInteger a(number);
+    BOOST_CHECK(a.GetSignForTests() && a.GetDigitsForTests() == "123");
+}
+
+BOOST_AUTO_TEST_CASE(TestConstructorUnsignedLong)
+{
+    unsigned long number = 432156789;
+    LongInteger a(number);
+    BOOST_CHECK(a.GetSignForTests() && a.GetDigitsForTests() == "432156789");
+}
+
+BOOST_AUTO_TEST_CASE(TestConstructorUnsignedLongLong)
+{
+    unsigned long long number = 8446744073709551616;
+    LongInteger a(number);
+    BOOST_CHECK(a.GetSignForTests() &&
+                a.GetDigitsForTests() == "8446744073709551616");
+}
+
+BOOST_AUTO_TEST_CASE(TestConstructorLongInteger)
+{
+    LongInteger a(5);
+    LongInteger b(a);
+    BOOST_CHECK(b.GetSignForTests() && b.GetDigitsForTests() == "5");
+}
+
