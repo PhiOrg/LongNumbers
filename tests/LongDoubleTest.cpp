29ff47a696684149ba2ea3ef32b92f417e21f746
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-08 16:54:10 +0300

    Added test for cast to LongInteger. Renamed GetDigitsForTest, GetDecimalsForTest, GetSignForTest and GetDecimalsNumberForTest in GetDigits, GetDecimals, GetSign, GetDecimalsNumber.

--- tests/LongDoubleTest.cpp
+++ tests/LongDoubleTest.cpp
@@ -9,15 +9,12 @@ BOOST_AUTO_TEST_CASE(TestLongDoubleConstructor)
 {
     LongDouble a(3.17), b(-1923.34646), c(56.123456789, 9);
 
-    BOOST_CHECK(a.GetDigitsForTest() == "3" &&
-                a.GetDecimalsForTest() == "170000" &&
-                a.GetSignForTest() && a.GetDecimalsNumberForTest() == 6);
-    BOOST_CHECK(b.GetDigitsForTest() == "1923" &&
-                b.GetDecimalsForTest() == "346460" &&
-                !b.GetSignForTest() && b.GetDecimalsNumberForTest() == 6);
-    BOOST_CHECK(c.GetDigitsForTest() == "56" &&
-                c.GetDecimalsForTest() == "123457000" &&
-                c.GetSignForTest() && c.GetDecimalsNumberForTest() == 9);
+    BOOST_CHECK(a.GetDigits() == "3" && a.GetDecimals() == "170000" &&
+                a.GetSign() && a.GetDecimalsNumber() == 6);
+    BOOST_CHECK(b.GetDigits() == "1923" && b.GetDecimals() == "346460" &&
+                !b.GetSign() && b.GetDecimalsNumber() == 6);
+    BOOST_CHECK(c.GetDigits() == "56" && c.GetDecimals() == "123457000" &&
+                c.GetSign() && c.GetDecimalsNumber() == 9);
 }
 
 BOOST_AUTO_TEST_CASE(TestLongIntegerConstructor)
@@ -25,11 +22,18 @@ BOOST_AUTO_TEST_CASE(TestLongIntegerConstructor)
     LongInteger a = 17, b = -32;
     LongDouble c(a), d(b, 9);
 
-    BOOST_CHECK(c.GetDigitsForTest() == "17" &&
-                c.GetDecimalsForTest() == "000000" &&
-                c.GetSignForTest() && c.GetDecimalsNumberForTest() == 6);
-    BOOST_CHECK(d.GetDigitsForTest() == "32" &&
-                d.GetDecimalsForTest() == "000000000" &&
-                !d.GetSignForTest() && d.GetDecimalsNumberForTest() == 9);
+    BOOST_CHECK(c.GetDigits() == "17" && c.GetDecimals() == "000000" &&
+                c.GetSign() && c.GetDecimalsNumber() == 6);
+    BOOST_CHECK(d.GetDigits() == "32" && d.GetDecimals() == "000000000" &&
+                !d.GetSign() && d.GetDecimalsNumber() == 9);
+}
+
+BOOST_AUTO_TEST_CASE(TestCastToLongInteger)
+{
+    LongDouble x(172.123), y(-172.123);
+    LongInteger a = (LongInteger) x, b = (LongInteger) y;
+
+    BOOST_CHECK(a.GetDigits() == "172" && a.GetSign());
+    BOOST_CHECK(b.GetDigits() == "172" && !b.GetSign());
 }
 
