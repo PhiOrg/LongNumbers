1a39edd5f2855555751869089301d1e6cad8ca12
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-08 14:05:16 +0300

    Added test for long double constructor

--- tests/LongDoubleTest.cpp
+++ tests/LongDoubleTest.cpp
@@ -5,3 +5,18 @@
 
 using namespace std;
 
+BOOST_AUTO_TEST_CASE(TestLongDoubleConstructor)
+{
+    LongDouble a(3.17), b(-1923.34646), c(56.123456789, 9);
+
+    BOOST_CHECK(a.GetDigitsForTest() == "3" &&
+                a.GetDecimalsForTest() == "170000" &&
+                a.GetSignForTest() && a.GetDecimalsNumberForTest() == 6);
+    BOOST_CHECK(b.GetDigitsForTest() == "1923" &&
+                b.GetDecimalsForTest() == "346460" &&
+                !b.GetSignForTest() && b.GetDecimalsNumberForTest() == 6);
+    BOOST_CHECK(c.GetDigitsForTest() == "56" &&
+                c.GetDecimalsForTest() == "123457000" &&
+                c.GetSignForTest() && c.GetDecimalsNumberForTest() == 9);
+}
+
