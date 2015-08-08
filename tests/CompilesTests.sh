c066298771906e12eb1db0f87cddaa452d01ddc7
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-08 15:23:17 +0300

    Removed -DTEST cflag

--- tests/CompilesTests.sh
+++ tests/CompilesTests.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -DTEST LongIntegerTest.cpp LongDoubleTest.cpp ../bin/LongNumber.a -lboost_unit_test_framework
+g++ -std=c++11 LongIntegerTest.cpp LongDoubleTest.cpp ../bin/LongNumber.a -lboost_unit_test_framework
