e3a37c4d82eed00c8d3d3badb9cd581f8c1aadab
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-08 16:56:55 +0300

    Added SplitNumber, GetSign, GetDigits, GetDecimals, GetDecimalsNumber and fixed the errors.

--- src/LongDouble.cpp
+++ src/LongDouble.cpp
@@ -2,40 +2,75 @@
 
 using namespace std;
 
-LongDouble::LongDouble(long double number, unsigned short int decimals)
+bool LongDouble::GetSign() const
 {
+    return sign;
+}
+
+const string& LongDouble::GetDigits() const
+{
+    return digits;
+}
+
+const string& LongDouble::GetDecimals() const
+{
+    return decimals;
+}
+
+unsigned short int LongDouble::GetDecimalsNumber() const
+{
+    return decimalsNumber;
+}
+
+void SplitNumber(string& digits, string& decimals, string number)
+{
+    bool ok = false;
+    size_t size = number.size(), i;
+    for (i = 0; i < size && number[i] != '.'; i++)
+        digits.push_back(number[i]);
+    i++;
+    for (; i < size; i++)
+        decimals.push_back(number[i]);
+}
+
+LongDouble::LongDouble(const long double number, unsigned short int decimals)
+{
+    long double x = number;
     if (number < 0)
     {
         sign = false;
-        number *= -1;
+        x *= -1;
     }
     else
         sign = true;
 
-    this->decimals = decimals;
-    digits = to_string(number);
+    decimalsNumber = decimals;
+    SplitNumber(digits, this->decimals, to_string(x));
+
+    if (decimalsNumber < this->decimals.size())
+    {
+        decimalsNumber = this->decimals.size();
+    }
+    else
+    {
+        for (size_t i = this->decimals.size(); i < decimalsNumber; i++)
+            this->decimals.push_back('0');
+    }
 }
 
 LongDouble::LongDouble(const LongInteger& number, unsigned short int decimals)
 {
-    digits = number.GetDigits() + ".0";
+    digits = number.GetDigits();
     sign = number.GetSign();
-    this->decimals = decimals;
+    decimalsNumber = decimals;
+    for (size_t i = 0; i < decimals; i++)
+        this->decimals.push_back('0');
 }
 
 LongDouble::operator LongInteger() const
 {
-    LongInteger number;
-    string d = digits;
-    size_t size = digits.size(), i;
-
-    for (i = 0; i < size; i++)
-        if (d[i] == '.')
-            break;
-    d.erase(i);
-
+    LongInteger number(digits);
     number.SetSign(sign);
-    number.SetDigits(d);
 
     return number;
 }
