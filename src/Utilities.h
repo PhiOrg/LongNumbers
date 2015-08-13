8c855da4081efa3c54b346e9cccdb31adce763e9
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-13 12:59:40 +0300

    Added CheckString()

--- src/Utilities.h
+++ src/Utilities.h
@@ -10,6 +10,8 @@ std::string GathersNumbers(const std::string&, const std::string&);
 
 std::string DecreasesNumbers(const std::string&, const std::string&);
 
+void CheckString(const std::string&, std::string&, bool&);
+
 } //end namespace
 
 #endif
