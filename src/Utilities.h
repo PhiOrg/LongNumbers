a16bbca4920abd7274721ea5df7218b20b38ba34
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-20 14:30:47 +0300

    Added little documentation.

--- src/Utilities.h
+++ src/Utilities.h
@@ -6,14 +6,32 @@
 namespace LongNumber
 {
 
+/*
+ * The first string must be greater than the second string or both strings to
+ * have the same size.
+ */
 std::string GathersNumbers(const std::string&, const std::string&);
 
+/*
+ * The first string must be greater than the second string.
+ */
 std::string DecreasesNumbers(const std::string&, const std::string&);
 
+/*
+ * Check if the string represents an integer number.
+ */
 void CheckString(const std::string&, std::string&, bool&);
 
+/*
+ * Return true if the string is not equal with 0, otherwise false.
+ */
 bool CheckIfStringNotEqualWithZero(const char*, size_t);
 
+/* Return:
+ * -1 if a <  b
+ *  0 if a == b
+ *  1 if a >  b
+ */
 short int CompareStrings(const std::string&, const std::string&);
 
 } //end namespace
