a371aa555d320b601094bd1ce8fb83e48c059b8a
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-26 02:23:43 +0300

    Added test for prefix operator--

--- tests/LongDoubleTest.cpp
+++ tests/LongDoubleTest.cpp
@@ -284,3 +284,22 @@ BOOST_AUTO_TEST_CASE(LongDoubleTestPrefixIncrementOperator)
     BOOST_CHECK(b == 5);
 }
 
+BOOST_AUTO_TEST_CASE(LongDoubleTestPrefixDecrementOperator)
+{
+    LongDouble a = -5, b = 4, c = 1, d = 0.2, e = -0.3, f = 1.4;
+
+    --a;
+    --b;
+    --c;
+    --d;
+    --e;
+    --f;
+
+    BOOST_CHECK(a == -6);
+    BOOST_CHECK(b == 3);
+    BOOST_CHECK(c == 0);
+    BOOST_CHECK(d == -0.8);
+    BOOST_CHECK(e == -1.3);
+    BOOST_CHECK(f == 0.4);
+}
+
