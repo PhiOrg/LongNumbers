2493a1f08f22b338aa8848f909d8ee4e2ddd640e
EnachescuAlin <alinenachescu033@gmail.com>
2015-09-13 22:00:18 +0300

    Added LongNumberMathTest.cpp. file.

--- tests/CompilesTests.sh
+++ tests/CompilesTests.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 LongIntegerTest.cpp LongDoubleTest.cpp ../bin/LongNumber.a -lboost_unit_test_framework
+g++ -std=c++11 LongIntegerTest.cpp LongDoubleTest.cpp LongNumberMathTest.cpp ../bin/LongNumber.a -lboost_unit_test_framework
